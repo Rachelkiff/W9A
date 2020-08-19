7e9fbc5bbc98c00c913d6a263191df9026da0f79
rachel <rachelkiffiak1@gmail.com>
2020-08-18 23:33:39 -0600

    Added variables and the booleans for W9A assignment

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var is_subscribed =true;
+var userAge =18;
+
+console.log(is_subscribed);
+console.log(userAge);
+
+if (userAge>18 ===is_subscribed) {
+    console.log("User is old enough to subscribe");
+} else if (userAge<18 ===is_subscribed) {
+    console.log("User is younger than 18 and not subscribed");
+} else if (userAge<18 !==is_subscribed)  {
+     console.log("User is younger than 18 and is subscribed!");
+} else if (userAge>=18 !=is_subscribed)  {
+      console.log ("User is 18 and older and is subscribed!");
+  }
+} else (userAge= is_subscribed)
+}
