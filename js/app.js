c2336a600cc23387990f2e1a5fe75910e22cd314
rachel <rachelkiffiak1@gmail.com>
2020-08-20 19:39:00 -0600

    Added the correct script in Index.html and fixed the code so that it will actually run.

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
-var is_subscribed =true;
-var userAge =18;
+var userAge = 18;
+var isSubscribed = false;
 
-console.log(is_subscribed);
+console.log(isSubscribed);
 console.log(userAge);
 
-if (userAge>18 ===is_subscribed) {
-    console.log("User is old enough to subscribe");
-} else if (userAge<18 ===is_subscribed) {
-    console.log("User is younger than 18 and not subscribed");
-} else if (userAge<18 !==is_subscribed)  {
-     console.log("User is younger than 18 and is subscribed!");
-} else if (userAge>=18 !=is_subscribed)  {
-      console.log ("User is 18 and older and is subscribed!");
-  }
-} else (userAge= is_subscribed)
+if (userAge >= 18 && isSubscribed) {
+    console.log("User is over 18 and is subscribed!");
+} else if (userAge < 18 && isSubscribed) {
+    console.log("User is under 18 and is subscribed!");
+} else if (userAge >= 18 && !isSubscribed)  {
+     console.log("User is 18 or older and is not subscribed!");
+} else if (userAge < 18 && !isSubscribed)  {
+      console.log("User is 18 or younger and is not subscribed!");
+} else {
+   console.log("Something went wrong!");
 }
